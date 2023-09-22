3067e0338c1b194bb75b87264116af046eaa942f
michelletaing <mleetaing@gmail.com>
2023-09-22 18:01:48 -0400

    created Board page with placeholder text

---
+++ frontend/src/pages/Board/Board.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { Typography } from '@mui/material';
+
+export default function Board() {
+  return (
+    <Typography variant='body'>This is the Board page.</Typography>
+  );
+}
