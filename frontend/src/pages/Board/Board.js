9ff596b2f10d13be14d873efc87c12a4bae76a59
michelletaing <mleetaing@gmail.com>
2023-09-24 14:59:51 -0400

    updated board information, adjusted spacing, and breakpoints

--- frontend/src/pages/Board/Board.js
+++ frontend/src/pages/Board/Board.js
@@ -8,93 +8,106 @@ export default function Board() {
     {
       name: 'Susana Bao',
       position: 'President',
-      blurb: 'This is a little awesome fun fact about me.',
-      avatar: 'susana-bao.jpg'
+      blurb: 'Computer Science',
+      avatar: 'susana-bao.jpg',
+      linkedin: 'https://www.linkedin.com/in/haohui-bao/'
     },
     {
       name: 'Christine Lin',
       position: 'President',
-      blurb: 'This is a little awesome fun fact about me.',
-      avatar: 'christine-lin.jpeg'
+      blurb: 'Computer Science',
+      avatar: 'christine-lin.jpeg',
+      linkedin: 'https://www.linkedin.com/in/lin-christine/'
     },
     {
       name: 'Carolina Boitel',
       position: 'Treasurer',
-      blurb: 'This is a little awesome fun fact about me.',
-      avatar: 'carolina-boitel.JPG'
+      blurb: 'Computer Science',
+      avatar: 'carolina-boitel.JPG',
+      linkedin: 'https://www.linkedin.com/in/carolina-boitel/'
     },
     {
       name: 'Sabrina Wong',
       position: 'VP of Curriculum Development',
-      blurb: 'This is a little awesome fun fact about me.',
-      avatar: 'sabrina-wong.jpeg'
+      blurb: 'Computer Engineering',
+      avatar: 'sabrina-wong.jpeg',
+      linkedin: 'https://www.linkedin.com/in/wongssabrina/'
     },
     {
       name: 'Kelly Chen',
       position: 'VP of External Affairs',
-      blurb: 'This is a little awesome fun fact about me.',
-      avatar: 'kelly-chen.jpg'
+      blurb: 'Computer Science',
+      avatar: 'kelly-chen.jpg',
+      linkedin: 'https://www.linkedin.com/in/kelly-chen-417128260/'
     },
     {
       name: 'Maddie Glaum',
       position: 'Director of Public Relations',
-      blurb: 'This is a little awesome fun fact about me.',
-      avatar: 'maddie-glaum.jpeg'
+      blurb: 'Computer Science',
+      avatar: 'maddie-glaum.jpeg',
+      linkedin: 'https://www.linkedin.com/in/madeleineglaum/'
     },
     {
       name: 'Savannah Fletcher',
       position: 'Director of Public Relations',
-      blurb: 'This is a little awesome fun fact about me.',
-      avatar: 'savannah-fletcher.jpg'
+      blurb: 'Computer Science',
+      avatar: 'savannah-fletcher.jpg',
+      linkedin: 'https://www.linkedin.com/in/savannah-fletcher-a56174251/'
     },
     {
       name: 'Michelle Taing',
       position: 'Webmaster',
-      blurb: 'This is a little awesome fun fact about me. I love to code and I love singing, playing guitar, and cooking.',
-      avatar: 'michelle-taing.jpg'
+      blurb: 'Computer Science',
+      avatar: 'michelle-taing.jpg',
+      linkedin: 'https://www.linkedin.com/in/michelletaing/'
     },
     {
       name: 'Stephanie Nguyen',
       position: 'Director of Events',
-      blurb: 'This is a little awesome fun fact about me.',
-      avatar: 'stephanie-nguyen.jpg'
+      blurb: 'Computer Science',
+      avatar: 'stephanie-nguyen.jpg',
+      linkedin: 'https://www.linkedin.com/in/stephanienguyen25/'
     },
     {
       name: 'Christina Nguyen',
       position: 'Director of Events',
-      blurb: 'This is a little awesome fun fact about me.',
-      avatar: 'christina-nguyen.jpeg'
+      blurb: 'Information Systems',
+      avatar: 'christina-nguyen.jpeg',
+      linkedin: 'https://www.linkedin.com/in/christinalenguyen/'
     },
     {
       name: 'Megan Shah',
       position: 'Director of Workshops',
-      blurb: 'This is a little awesome fun fact about me.',
-      avatar: 'megan-shah.jpg'
+      blurb: 'Computer Science',
+      avatar: 'megan-shah.jpg',
+      linkedin: 'https://www.linkedin.com/in/megan-shah/'
     },
   ];
   
   return (
     <div>
       <Typography 
-        variant="h2" 
+        variant="h3" 
         pt={2} 
-        pl={3} 
-        sx={{ fontFamily: 'DM Sans', textAlign: 'left', color: '#1A1421' }}
+        pl={{ xs: 2, md: 4 }} 
+        sx={{ fontFamily: 'DM Sans', textAlign: 'left', color: '#1A1421', fontWeight: '700' }}
       >
-        Our Board
+        OUR BOARD
       </Typography>
       <Grid 
         container 
         spacing={2}
-        sx={{ px: 3, py: 3 }} 
+        px={{ xs: 2, md: 4}}
+        py={2}
       >
         {members.map((member, index) => (
-          <Grid item xs={12} md={3} key={index}>
+          <Grid item xs={12} sm={6} md={3} key={index}>
             <BoardCard
               name={member.name}
               position={member.position}
               blurb={member.blurb}
               avatar={member.avatar}
+              linkedin={member.linkedin}
             />
           </Grid>
         ))}
