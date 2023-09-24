fde6c6bffe02445da1dece3f259ae44421c31035
michelletaing <mleetaing@gmail.com>
2023-09-24 14:59:01 -0400

    rearranged elements, adjustments on spacing, and added linkedin hyperlink

--- frontend/src/components/BoardCard/BoardCard.js
+++ frontend/src/components/BoardCard/BoardCard.js
@@ -6,7 +6,7 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 
-export default function BoardCard({ name, position, blurb, avatar }) {
+export default function BoardCard({ name, position, blurb, avatar, linkedin }) {
   return (
     <Card style={{ boxShadow: 'none', borderRadius: '30px' }}>
       <CardMedia
@@ -17,28 +17,38 @@ export default function BoardCard({ name, position, blurb, avatar }) {
         alt="avatar"
       />
       <CardContent>
-        <Grid container style={{ paddingBottom: '15px'}} >
+        <Grid container style={{ paddingBottom: '15px'}}>
             <Grid item xs={12} md={12}>
-                <Typography gutterBottom variant="h5" component="div" style={{ fontFamily: 'DM Sans', textAlign: 'left' }}>
+                <Typography 
+                  gutterBottom 
+                  variant="h5" 
+                  mb={1} 
+                  p={0} 
+                  component="div" 
+                  style={{ fontFamily: 'DM Sans', fontWeight: '500', textAlign: 'left' }}
+                >
+                  <a href={ linkedin } style={{ textDecoration: 'none', color: 'inherit', borderBottom: '2px solid #ffe45e' }} target='_blank'>
                     { name }
+                  </a>
+                    
                 </Typography>
             </Grid>
             <Grid item xs={12} md={12}>
-                <div style={{ textAlign: 'left' }}>
-                    <Button 
-                        variant="outlined" 
-                        size="medium" 
-                        sx={{ fontFamily: 'DM Sans', color: '#BE9BCB', borderColor: '#BE9BCB', borderRadius: '12px', pointerEvents: 'none', width: '100%' }}
-                    >
-                        { position }
-                    </Button>
-                </div>
-                
+              <Typography variant="body2" color="text.secondary" style={{ fontFamily: 'DM Sans', textAlign: 'left' }}>
+                { blurb }
+              </Typography>
             </Grid>
         </Grid>
-        <Typography variant="body2" color="text.secondary" style={{ fontFamily: 'DM Sans', textAlign: 'left' }}>
-          { blurb }
-        </Typography>
+        <div style={{ textAlign: 'left' }}>
+          <Button 
+              variant="outlined" 
+              size="medium" 
+              sx={{ fontFamily: 'DM Sans', color: '#BE9BCB', borderColor: '#BE9BCB', borderRadius: '12px', pointerEvents: 'none', width: '100%' }}
+          >
+              { position }
+          </Button>
+        </div>
+        
       </CardContent>
     </Card>
   );
