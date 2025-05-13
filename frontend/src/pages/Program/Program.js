e348bc95aee0450956f9f99510e3afa2c6e5e616
tanvi <tanvigarg.nj@gmail.com>
2025-05-12 23:32:37 -0400

    cs kickoff 2025 updates

--- frontend/src/pages/Program/Program.js
+++ frontend/src/pages/Program/Program.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import Banner from '../../assets/cs-kickoff-banner.png';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import Box from '@mui/material/Box';
@@ -10,27 +9,31 @@ import { Button, Typography } from '@mui/material';
 import Recap1 from '../../assets/recap-1.jpg';
 import Recap2 from '../../assets/recap-2.jpg';
 import Recap3 from '../../assets/recap-3.jpg';
-import Recap4 from '../../assets/recap-4.jpg';
-import Recap5 from '../../assets/recap-5.jpg';
-import Recap6 from '../../assets/recap-6.jpg';
-
+import Board from '../../assets/board.jpg';
+import Marshmallow from '../../assets/marshmallow.png';
+import Group from '../../assets/group.png';
+import Selfie from '../../assets/selfie.jpg';
+import Presentation from '../../assets/presentation.png';
+import CSKickoff2024 from '../../assets/cskickoff2024.jpg';
 function Program() {
     const itemData = [
         {
-            img: Recap4
+            img: Selfie
         },
         {
-            img: Recap1
+            img: Marshmallow
         },
         {
-            img: Recap2
+            img: Board
         },
         {
-            img: Recap3
+            img: Group
         },
+      
         {
-            img: Recap6
-        }
+            img: Presentation
+        },
+       
     ];
 
     return (
@@ -38,27 +41,35 @@ function Program() {
         {/* <img src={ Banner } alt="CS Kickstart logo with text." style={{ width: '100%' }}/> */}
 
         <Box
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-        >
-        <ImageList
-            sx={{ width: '100%', height: 'auto' }}
-            cols={5} // Dynamically adjusts based on screen size
-            rowHeight={{ xs: 200, sm: 300 }} // Smaller height for mobile
-        >
-            {itemData.map((item) => (
-                <ImageListItem key={item.img}>
-                    <img
-                    srcSet={`${item.img}?w=300&h=300&fit=crop&auto=format&dpr=2 2x`}
-                    src={`${item.img}?w=300&h=300&fit=crop&auto=format`}
-                    alt={item.title}
-                    loading="lazy"
-                    />
-                </ImageListItem>
-            ))}
-        </ImageList>
-        </Box>
+  display="flex"
+  justifyContent="center"
+  alignItems="center"
+>
+  <ImageList
+    sx={{ width: '100%', height: 'auto' }}
+    cols={6}
+    gap={0}
+  >
+    {itemData.map((item, index) => {
+      let cols = 1;
+
+      if (index === 2) {
+        cols = 2;
+      }
+
+      return (
+        <ImageListItem key={item.img} cols={cols}>
+          <img
+            srcSet={`${item.img}?w=${300 * cols}&h=300&fit=crop&auto=format&dpr=2 2x`}
+            src={`${item.img}?w=${300 * cols}&h=300&fit=crop&auto=format`}
+            alt={item.title}
+            loading="lazy"
+          />
+        </ImageListItem>
+      );
+    })}
+  </ImageList>
+</Box>
 
         <Grid 
                 container 
@@ -67,6 +78,51 @@ function Program() {
                 px={3}
                 py={{ xs: 2, md: 4 }}
             >
+                <Grid item xs={12} md={4}>
+                <Typography
+                    sx={{
+                        textAlign: {
+                            xs: 'left',
+                            md: 'right',
+                        },
+                        fontFamily: 'DM Sans',
+                        fontWeight: '700',
+                        color: '#1A1421',
+                        letterSpacing: '2px'
+                    }}
+                    variant='h4'
+                >
+                    APPLY
+                </Typography>
+            </Grid>
+            <Grid item xs={12} md={6} style={{ paddingBottom: '20px' }}>
+                    {/* <Typography 
+                        style={{ fontFamily: 'DM Sans', margin: '0', paddingBottom: '20px' }}
+                        align='left'
+                    >
+                        Applications for our summer 2025 program will be opening at the end of the spring 2025 semester.
+                    </Typography> */}
+                    <div style={{ textAlign: 'left' }}>
+                        <Button 
+                            className="button-text" 
+                            variant="contained" 
+                            size="large"
+                            style={{ backgroundColor: '#fea5b0', boxShadow: 'none', fontFamily: 'DM Sans' }} 
+                            href='https://docs.google.com/forms/d/e/1FAIpQLSfP5NQ8Tt8cJLdFO4sF6HwRaMX-O7dS4kWXoMukZYtkCJr80w/viewform'
+                            target='_blank'
+                            rel="noreferrer"
+                        >
+                                2025 CS Kickoff Application Form
+                        </Button>
+                    </div>
+                    <Typography 
+                    style={{ fontFamily: 'DM Sans', margin: '0', paddingBottom: '20px' }}
+                    align='left'
+                >
+                    <br></br>
+                    Applications close July 20th, 2025 at 11:59 PM EST, so be sure to apply before then!
+                </Typography>
+            </Grid>
             <Grid item xs={12} md={4}>
                 <Typography
                     sx={{
@@ -89,57 +145,22 @@ function Program() {
                     style={{ fontFamily: 'DM Sans', margin: '0', paddingBottom: '20px' }}
                     align='left'
                 >
-                    CS Kickoff is a free, multi-day program from <i>Sunday, August 18, 2024 to Tuesday, August 20, 2024</i> created to inspire and empower women in technology. 
+                    CS Kickoff is a free, multi-day program from Monday, August 18, 2025 to Wednesday, August 20, 2025 created to inspire and empower beginners in technology. 
 
                     <br></br>
                     <br></br>
 
-                    Tailored specifically for incoming women students at the University of Florida who have a passion for math, science, or engineering, our program provides participants a unique opportunity to get a head-start on the world of Computer Science before the fall semester begins.
+                    Tailored specifically for incoming students at the University of Florida who have a passion for math, science, or engineering, our program provides participants a unique opportunity to get a head-start on the world of Computer Science before the fall semester begins.
 
                     <br></br>
                     <br></br>
 
-                    Our goal is to cultivate a diverse and inclusive tech community by providing hands-on learning opportunities, mentorship, and exposure to industry practices. 
+                    Our goal is to cultivate a diverse and inclusive tech community by providing hands-on learning opportunities, mentorship, and exposure to industry practices.
+ 
                 </Typography>
             </Grid>
-            <Grid item xs={12} md={4}>
-                <Typography
-                    sx={{
-                        textAlign: {
-                            xs: 'left',
-                            md: 'right',
-                        },
-                        fontFamily: 'DM Sans',
-                        fontWeight: '700',
-                        color: '#1A1421',
-                        letterSpacing: '2px'
-                    }}
-                    variant='h4'
-                >
-                    RECAP
-                </Typography>
-            </Grid>
-            <Grid item xs={12} md={6}>
-                <Typography 
-                    style={{ fontFamily: 'DM Sans', margin: '0', paddingBottom: '20px' }}
-                    align='left'
-                >
-                    During our Summer 2024 program, we welcomed an incredible group of young women to UF! We dived into student panels, hands-on workshops, and inspiring talks from industry leaders and a UF professor. Our attendees also explored tech-related clubs, connected at socials, and so much more! 🫶
-
-                    <br></br>
-                    <br></br>
-
-                    Here's what the stats looked like:
-                    <List sx={{ listStyleType: 'disc', pl: 4, pt: 0 }}>
-                        <ListItem sx={{ display: 'list-item', pb: 0 }}>3 days</ListItem>
-                        <ListItem sx={{ display: 'list-item', pb: 0 }}>20 participants</ListItem>
-                        <ListItem sx={{ display: 'list-item', pb: 0 }}>6 workshops</ListItem>
-                        <ListItem sx={{ display: 'list-item', pb: 0 }}>3 panels</ListItem>
-                    </List>
-
-                    Here’s to building a brighter tech future together! 🚀👩‍💻
-                </Typography>
-            </Grid>
+            
+            
             <Grid item xs={12} md={4}>
                 <Typography
                     sx={{
@@ -200,6 +221,7 @@ function Program() {
                             <ListItem sx={{ display: 'list-item', pb: 0 }}>You are interested in acquiring foundational programming skills and gaining hands-on experience in coding.</ListItem>
                             <ListItem sx={{ display: 'list-item', pb: 0 }}>You want to expand your knowledge in technology and engineering through immersive and hands-on experiences.</ListItem>
                             <ListItem sx={{ display: 'list-item', pb: 0 }}>You are seeking a supportive and inclusive community at the University of Florida.</ListItem>
+                            <ListItem sx={{ display: 'list-item', pb: 0 }}>You want to have fun!</ListItem>
                         </List>
                     </Typography>
             </Grid>
@@ -229,10 +251,11 @@ function Program() {
                         <List sx={{ listStyleType: 'disc', pl: 4, pt: 0 }}>
                             <ListItem sx={{ display: 'list-item', pb: 0 }}><strong>Workshops & Labs:</strong> In our workshops, you will be able to get exposure and hands-on experience in Python, machine learning, and Arduino boards! This is a great opportunity to learn and get a good foundation of both sides of the tech world, software and hardware. Whether you are a beginner or looking to enhance your skills, our workshops are designed to cater to all levels.</ListItem>
                             <ListItem sx={{ display: 'list-item', pb: 0 }}><strong>Socials:</strong> You will be able to participate in many fun activities that will allow you to get to know other attendees and CSK members. This is a great way to start building your network of connections and friends who can help support you through the years and become a community you can rely on.</ListItem>
-                            <ListItem sx={{ display: 'list-item', pb: 0 }}><strong>Panels:</strong> You will be able to interact with and ask questions to our various speakers. We will feature engineers discussing industry opportunities, professors sharing their research and offering preparation tips, upper-classmen answering college life questions, and representatives from various organizations helping you explore where you fit best.</ListItem>
+                            <ListItem sx={{ display: 'list-item', pb: 0 }}><strong>Panels:</strong> You will be able to interact with and ask questions to our various speakers. We will feature engineers discussing industry opportunities, professors sharing their research and offering preparation tips, upper-classmen answering college life questions, and representatives from various organizations helping you explore where you fit best. </ListItem>
                         </List>                    
                     </Typography>
             </Grid>
+            
             <Grid item xs={12} md={4}>
                 <Typography
                     sx={{
@@ -247,31 +270,30 @@ function Program() {
                     }}
                     variant='h4'
                 >
-                    APPLY
+                    RECAP
                 </Typography>
             </Grid>
-            <Grid item xs={12} md={6} style={{ paddingBottom: '20px' }}>
-                    <Typography 
-                        style={{ fontFamily: 'DM Sans', margin: '0', paddingBottom: '20px' }}
-                        align='left'
-                    >
-                        Applications for our summer 2025 program will be opening at the end of the spring 2025 semester.
-                    </Typography>
-                    {/* <div style={{ textAlign: 'left' }}>
-                        <Button 
-                            className="button-text" 
-                            variant="contained" 
-                            size="large"
-                            style={{ backgroundColor: '#fea5b0', boxShadow: 'none', fontFamily: 'DM Sans' }} 
-                            href='https://docs.google.com/forms/d/e/1FAIpQLSd-CRsPsEzvSC1gAufZ3XZ4ot60fjFSA3hmrdAIGSIMD-J7Hg/viewform'
-                            target='_blank'
-                            rel="noreferrer"
-                        >
-                                APPLY HERE
-                        </Button>
-                    </div> */}
+            <Grid item xs={12} md={6}>
+                <Typography 
+                    style={{ fontFamily: 'DM Sans', margin: '0', paddingBottom: '20px' }}
+                    align='left'
+                >
+                    During our Summer 2024 program, we welcomed an incredible group of young women to UF! We dived into student panels, hands-on workshops, and inspiring talks from industry leaders and a UF professor. Our attendees also explored tech-related clubs, connected at socials, and so much more! 🫶
+
+                    <br></br>
+                    <br></br>
+
+                    Here's what the stats looked like:
+                    <List sx={{ listStyleType: 'disc', pl: 4, pt: 0 }}>
+                        <ListItem sx={{ display: 'list-item', pb: 0 }}>3 days</ListItem>
+                        <ListItem sx={{ display: 'list-item', pb: 0 }}>20 participants</ListItem>
+                        <ListItem sx={{ display: 'list-item', pb: 0 }}>6 workshops</ListItem>
+                        <ListItem sx={{ display: 'list-item', pb: 0 }}>3 panels</ListItem>
+                    </List>
+
+                    Here’s to building a brighter tech future together! 🚀👩‍💻
+                </Typography>
             </Grid>
-            
 
         </Grid>
     </div>
