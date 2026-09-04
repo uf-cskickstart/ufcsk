a257e3d8b1a0e4b6dabbd370ac2ad06eacf4f30e
teafany <Tiffany121705@gmail.com>
2026-09-04 09:47:38 -0400

    run prettier on changed files

--- frontend/src/components/Event/Event.js
+++ frontend/src/components/Event/Event.js
@@ -6,29 +6,75 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import './Event.css';
 
-export default function Event({ title, date, time, description, location, link }) {
+export default function Event({
+  title,
+  date,
+  time,
+  description,
+  location,
+  link,
+}) {
   return (
-    <Card style={{ fontFamily: 'DM Sans', textAlign: 'left', borderRadius: '18px', boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px' }}>
+    <Card
+      style={{
+        fontFamily: 'DM Sans',
+        textAlign: 'left',
+        borderRadius: '18px',
+        boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px',
+      }}
+    >
       <CardContent style={{ margin: '8px' }}>
-        <Typography gutterBottom variant="body" component="div" style={{ color: '#BE9BCB' }}>
-          { date }{ time ? ` • ${time}` : '' }
+        <Typography
+          gutterBottom
+          variant="body"
+          component="div"
+          style={{ color: '#BE9BCB' }}
+        >
+          {date}
+          {time ? ` • ${time}` : ''}
         </Typography>
-        <Typography gutterBottom variant="h5" component="div" style={{ fontFamily: 'DM Sans' }}>
-          <a href={ link } style={{ textDecoration: 'none', color: 'inherit', borderBottom: '2px solid #ffe45e' }} target='_blank' rel="noreferrer">
-            { title }
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="div"
+          style={{ fontFamily: 'DM Sans' }}
+        >
+          <a
+            href={link}
+            style={{
+              textDecoration: 'none',
+              color: 'inherit',
+              borderBottom: '2px solid #ffe45e',
+            }}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {title}
           </a>
         </Typography>
-        { location && (
-          <Typography gutterBottom variant="body" component="div" style={{ fontFamily: 'DM Sans' }}>
-            <FontAwesomeIcon icon={ faLocationDot } style={{paddingRight: '8px'}} />
-            { location }
+        {location && (
+          <Typography
+            gutterBottom
+            variant="body"
+            component="div"
+            style={{ fontFamily: 'DM Sans' }}
+          >
+            <FontAwesomeIcon
+              icon={faLocationDot}
+              style={{ paddingRight: '8px' }}
+            />
+            {location}
           </Typography>
-        ) }
-        { description && (
-          <Typography variant="body2" color="text.secondary" style={{ fontFamily: 'DM Sans', paddingTop: '10px' }}>
-            { description }
+        )}
+        {description && (
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            style={{ fontFamily: 'DM Sans', paddingTop: '10px' }}
+          >
+            {description}
           </Typography>
-        ) }
+        )}
       </CardContent>
     </Card>
   );
